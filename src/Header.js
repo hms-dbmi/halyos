722e23fe19662f2aed7a33281392bb16090b208a
Vimig Socrates <vsocrates@Socratess-MacBook-Pro.local>
2017-07-13 02:35:45 -0400

    added react-bootstrap module and chartlets library

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
+import { Navbar, NavDropdown, MenuItem, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Header extends Component {
 
@@ -11,17 +13,35 @@ class Header extends Component {
 
   }
 
+  on
+
   render() {
     return (
-      <header>
-          <nav>
-          <ul>
-         <li><Link to='/'>Home</Link></li>
-         <li><Link to='/measure/test'>Measurements</Link></li>
-         <li><Link to='/risk'>Risk Scores</Link></li>
-       </ul>
-     </nav>
-   </header>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Ignite FHIR</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavDropdown eventKey={3} title="Measurements" id="basic-nav-dropdown">
+              <LinkContainer to="/measure/test"><MenuItem eventKey={3.1}>Action</MenuItem></LinkContainer>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            </NavDropdown>
+            <NavDropdown eventKey={3} title="Risk Scores" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
