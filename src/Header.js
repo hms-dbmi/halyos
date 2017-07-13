86806e86a50bd549a57ec813d516b0ad88892596
Vimig Socrates <vsocrates@socratess-mbp.private.wireless.med.harvard.edu>
2017-07-13 17:32:34 -0400

    finished header measurement selection

--- src/Header.js
+++ src/Header.js
@@ -10,14 +10,68 @@ class Header extends Component {
 
   constructor(props){
     super(props); 
+    this.state = {observations:null};
 
   }
 
-  on
+  componentWillMount() {
+    //a Set is used below for unique observation codes, could be {} probably.
+    this.observationList = [];
+    this.props.ptapi.fetchAll({type: "Observation", query:{$sort: [['code','asc']], _elements:['code']}})
+              .done(function(data){
+                //console.log(data);
+                let observationList = new Set();
+                let updatedList = []
+                for (let i = 0; i < data.length; i++){
+                  //adding stringified text so they can be compared for equality and we keep all the info, just JSON parse it
+                  observationList.add(JSON.stringify(data[i].code.coding[0]));
+                }
+                for (let item of observationList){
+                  updatedList.push(JSON.parse(item));
+                }
+                
+                this.setState({observations:updatedList})
+            
+            }.bind(this));
+    // console.log('list', observationList);
+    // for (let item of observationList){
+    //   console.log("item: ", item)
+    //   this.observationList.push(JSON.parse(item));
+    // }
+
+  }
 
   render() {
+    if(!this.state.observations){
+      return (
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Ignite FHIR</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavDropdown eventKey={3} title="Measurements" id="basic-nav-dropdown">
+                <LinkContainer to="/"><MenuItem eventKey={3.1}>Loading...</MenuItem></LinkContainer>
+              </NavDropdown>
+              <NavDropdown eventKey={3} title="Risk Scores" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Cardiavascular Disease</MenuItem>
+                <MenuItem eventKey={3.2}>Stroke</MenuItem>
+                <MenuItem eventKey={3.3}>Kidney Failure</MenuItem>
+                <MenuItem eventKey={3.4}>COPD</MenuItem>
+                <MenuItem eventKey={3.5}>Diabetes</MenuItem>
+              </NavDropdown>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      )
+    }
+    console.log("asdfasdfasfqweqdafsqwerasd", this.state.observations);
     return (
-      <Navbar fixed collapseOnSelect>
+      <Navbar collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="/">Ignite FHIR</a>
@@ -27,16 +81,19 @@ class Header extends Component {
         <Navbar.Collapse>
           <Nav>
             <NavDropdown eventKey={3} title="Measurements" id="basic-nav-dropdown">
-              <LinkContainer to="/measure/test"><MenuItem eventKey={3.1}>Action</MenuItem></LinkContainer>
-              <LinkContainer to="/measure/vimig"><MenuItem eventKey={3.2}>Another action</MenuItem></LinkContainer>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              { 
+                this.state.observations.map(function(obs){
+                  var link = "/measure/" + obs.code;
+                  return <LinkContainer to={link}><MenuItem eventKey={obs.code}>{obs.display}</MenuItem></LinkContainer>  
+                })  
+              }
             </NavDropdown>
             <NavDropdown eventKey={3} title="Risk Scores" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              <MenuItem eventKey={3.1}>Cardiavascular Disease</MenuItem>
+              <MenuItem eventKey={3.2}>Stroke</MenuItem>
+              <MenuItem eventKey={3.3}>Kidney Failure</MenuItem>
+              <MenuItem eventKey={3.4}>COPD</MenuItem>
+              <MenuItem eventKey={3.5}>Diabetes</MenuItem>
             </NavDropdown>
 
           </Nav>
--- src/Main.js
+++ src/Main.js
@@ -10,10 +10,7 @@ class Main extends Component {
   constructor(props){
     super(props); 
     console.log("thsese are them: ", props)
-    this.props.ptapi.fetchAll({type: "Observation", query:{$sort: [['code','asc']]}})
-	      			.then(function(r){ 
-	        			console.log("alsjdhfalsdkfjhalsdfjhasldfkjasldfajshd ",JSON.stringify(r,null,2));
-		    		});
+    
   }
 
   render() {
