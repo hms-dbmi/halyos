1f601a71da95d0c05ddecea791a28044e791159d
samson920 <samson.mataraso@berkeley.edu>
2017-07-25 14:01:36 -0400

    adding text file

---
+++ src/measurement/Measurement_Text.js
@@ -0,0 +1,16 @@
+export default {
+	text: {
+		
+		'2085-9': { //HDL
+			meaning: "This is what HDL means!",
+			affect: "This is what affects HDL!",
+			improve: "Here's how you can lose HDL!"
+		},
+
+		'2093-3': { //Cholesterol
+			meaning: "This is what cholesterol means!",
+			affect: "This is what affects cholesterol!",
+			improve: "Here's how you can lose cholesterol!"
+		}
+	}		
+};
