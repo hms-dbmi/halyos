beafcc70609ec1e3e63815d7d1cc294ab38f61a3
samson920 <samson.mataraso@berkeley.edu>
2017-07-28 13:41:59 -0400

    initial commit

--- src/measurement/Measurement_View.js
+++ src/measurement/Measurement_View.js
@@ -161,7 +161,11 @@ class MeasurementView extends Component {
 				<div>
 					<div className="row">
 						<div className="col-md-6">	
-							<PastGraph obs_data={this.state.measurementList} units={this.state.units}/>
+							<PastGraph obs_data={this.state.measurementList} units={this.state.units} 
+							mainWidth={500}
+							mainHeight={200}
+							viewWidth={500}
+							viewHeight={50}/>
 							<text style={{fontSize: "20", fontFamily: "HiraKakuStd-W8, Hiragino Kaku Gothic Std"}}>
 								Risk Scores Affected By This Measurement
 							</text>
@@ -237,7 +241,7 @@ class MeasurementText extends Component {
 			this.setState({
 				meaning: "No data available. Please consult your doctor.",
 				important: "No data available. Please consult your doctor.",
-				improve: "No data available. Please consult your doctor."
+				improvec: "No data available. Please consult your doctor."
 			});
 		}
 	}
