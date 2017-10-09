6e8e6646e9d2e9b1aaad2ecdfc1510693b4bbf82
Fritz Lekschas <service@lekschas.de>
2017-10-09 17:34:36 -0400

    Fix broken JSX template and switch to spaces instead of tabs

--- src/views/profile/Profile_View.js
+++ src/views/profile/Profile_View.js
@@ -38,97 +38,76 @@ import { medListStyle, medListDivStyle } from './Profile_View-style.js'
 //import AboutRisk from '../risk/AboutRisk.js';
 
 class ProfileView extends Component {
-	constructor(props){
-		super(props);
-	}
+  constructor(props){
+    super(props);
+  }
 
-	render(){ //Known issue; the code can easily be changed, the icon not so much....
-		//var ptLoc = getPtLoc(this.props.patient);
-		var ptLoc = {"country_code":"US","region_code":"MA","city":"Pepperell","zip_code":"01463","latitude":42.669838,"longitude":-71.5961267};
-		var patient = {"gender": "Male", "birthDate":'1979-02-03-12:45'};
-		var measurements = [{"name": "Systolic Blood Pressure", "units": "mmHg", "past": "120", "present": "110" },
-		{"name": "Diastolic Blood Pressure", "units": "mmHg", "past": "90", "present": "95" },
-		{"name": "Heart Rate", "units": "bpm", "past": "90", "present": "70" },
-		{"name": "Respiration Rate", "units": "breaths/min", "past": "18", "present": "18" }]
-		var mappedMeasures = measurements.map((measurements) => 
-			<tr className = "pure-table pure-table-horizontal">
-				<td> {measurements["name"]} [{measurements["units"]}] </td>
-				<td> {measurements["past"]}</td>
-				<td> {measurements["present"]}</td>
-				<td> :) </td>
-			</tr>
-		);
-		return (
-			<div>
-				<div className="pure-g">
-					<div className="pure-u-1-5"><RiskTile scoreName="General Cardiac" score={10} sym="%" context="within 10 years" url="General_Cardiac"/> </div>
-					<div className="pure-u-1-5"><RiskTile scoreName="Stroke" score={10} sym="%" context="within 1 year" url="Stroke"/></div>
-					<div className="pure-u-1-5"><RiskTile scoreName="Kidney Failure" score={10} sym="%" context="within 5 years" url="Kidney_Failure"/></div>
-					<div className="pure-u-1-5"><RiskTile scoreName="COPD Mortality" score={10} sym="%" context="within 4 years" url="COPD_Mortality"/></div>
-					<div className="pure-u-1-5"><RiskTile scoreName="Diabetes" score={10} sym="%" context="within 5 years" url="Diabetes"/></div>
-				</div>
-				<br/><br/>
-				<div>
-					<div className="pure-u-1-2" style={{"padding-left":"2px", "padding-right":"20px", "height":"300px", "overflow":"auto"}}>
-						<FilteredList measurements={measurements}/>
-					</div>
-					<div className="pure-u-8-24">
-						<AppointmentsTile patient={patient}/>
-					</div>
-					<div className="pure-u-4-24">
-
-							<div style={{"order":"2"}}>
-								<div style={envTileStyle}>
-									<PollenContainer location={ptLoc} />
-						<div style={{"display":"flex", "flex-direction":"row", "justify-content":"center"}}>
-							<div style={{"display":"flex", "flex-direction":"column", "justify-content": "center"}}>
-								<div style={{"textAlign":'center', "fontSize": "20", "order":"1"}}>
-									Environment
-								</div>
-								<div style={{"order":"2"}}>
-									<div style={envTileStyle}>
-										<PollenContainer location={ptLoc} />
-									</div>
-								</div>
-								<div style={{"order":"3"}}>
-									<div style={envTileStyle}>
-										<AirQuality location={ptLoc} />
-									</div>
-								</div>
-								<div style={{"order":"4"}}>
-									<div style={envTileStyle}>
-										<Flu location={ptLoc} />
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				{/*<div className="row">
-					<div className = "col-sm-2">
-        				<RiskTile scoreName="General Cardiac" score={reynoldsScore(pt, obs)} sym="%" context="within 10 years" url="General_Cardiac"/>
-					</div>
-					<div className = "col-sm-2">
-						<RiskTile scoreName="Stroke" score={CHADScore(pt, conds)} sym="%" context="within 1 year" url="Stroke"/>
-					</div>
-					<div className = "col-sm-2">
-	        			<RiskTile scoreName="Kidney Failure" score={KFRScore(pt, obs)} sym="%" context="within 5 years" url="Kidney_Failure"/>
-					</div>
-					<div className = "col-sm-2">
-	        			<RiskTile scoreName="COPD Mortality" score={COPDScore(pt, obs, conds)} sym="%" context="within 4 years" url="COPD_Mortality"/>
-					</div>
-					<div className = "col-sm-2">
-	        			<RiskTile scoreName="Diabetes" score={diabetesScore(pt, obs, conds, medreq)} sym="%" context="within 5 years" url="Diabetes"/>
-					</div>
-			
-					<div className = "col-sm-2">
-						<div><HelpRiskTile scoreName="Help"/></div>
-					</div>
-				</div>*/}
-			</div>
-
-		)
-	}
+  render(){ //Known issue; the code can easily be changed, the icon not so much....
+    //var ptLoc = getPtLoc(this.props.patient);
+    var ptLoc = {"country_code":"US","region_code":"MA","city":"Pepperell","zip_code":"01463","latitude":42.669838,"longitude":-71.5961267};
+    var patient = {"gender": "Male", "birthDate":'1979-02-03-12:45'};
+    var measurements = [{"name": "Systolic Blood Pressure", "units": "mmHg", "past": "120", "present": "110" },
+    {"name": "Diastolic Blood Pressure", "units": "mmHg", "past": "90", "present": "95" },
+    {"name": "Heart Rate", "units": "bpm", "past": "90", "present": "70" },
+    {"name": "Respiration Rate", "units": "breaths/min", "past": "18", "present": "18" }]
+    var mappedMeasures = measurements.map((measurements) =>
+      <tr className = "pure-table pure-table-horizontal">
+        <td> {measurements["name"]} [{measurements["units"]}] </td>
+        <td> {measurements["past"]}</td>
+        <td> {measurements["present"]}</td>
+        <td> :) </td>
+      </tr>
+    );
+    return (
+      <div>
+        <div className="pure-g">
+          <div className="pure-u-1-5"><RiskTile scoreName="General Cardiac" score={10} sym="%" context="within 10 years" url="General_Cardiac"/> </div>
+          <div className="pure-u-1-5"><RiskTile scoreName="Stroke" score={10} sym="%" context="within 1 year" url="Stroke"/></div>
+          <div className="pure-u-1-5"><RiskTile scoreName="Kidney Failure" score={10} sym="%" context="within 5 years" url="Kidney_Failure"/></div>
+          <div className="pure-u-1-5"><RiskTile scoreName="COPD Mortality" score={10} sym="%" context="within 4 years" url="COPD_Mortality"/></div>
+          <div className="pure-u-1-5"><RiskTile scoreName="Diabetes" score={10} sym="%" context="within 5 years" url="Diabetes"/></div>
+        </div>
+        <br/><br/>
+        <div>
+          <div className="pure-u-1-2" style={{"padding-left":"2px", "padding-right":"20px", "height":"300px", "overflow":"auto"}}>
+            <FilteredList measurements={measurements}/>
+          </div>
+          <div className="pure-u-8-24">
+            <AppointmentsTile patient={patient}/>
+          </div>
+          <div className="pure-u-4-24">
+            <div style={{"order":"2"}}>
+              <div style={envTileStyle}>
+                <PollenContainer location={ptLoc} />
+                <div style={{"display":"flex", "flex-direction":"row", "justify-content":"center"}}>
+                  <div style={{"display":"flex", "flex-direction":"column", "justify-content": "center"}}>
+                    <div style={{"textAlign":'center', "fontSize": "20", "order":"1"}}>
+                      Environment
+                    </div>
+                    <div style={{"order":"2"}}>
+                      <div style={envTileStyle}>
+                        <PollenContainer location={ptLoc} />
+                      </div>
+                    </div>
+                    <div style={{"order":"3"}}>
+                      <div style={envTileStyle}>
+                        <AirQuality location={ptLoc} />
+                      </div>
+                    </div>
+                    <div style={{"order":"4"}}>
+                      <div style={envTileStyle}>
+                        <Flu location={ptLoc} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
 }
 
