85edb258a2f8ccce99a7dadd272f1569a2ad5552
Vimig Socrates <vimig.socrates@gmail.com>
2017-10-12 19:21:52 -0400

    rerouted files in reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import { fhirPatientData } from './services/fhir/FhirReducers'
-import { envFactorsData } from './views/profile/env/EnvReducers'
+import { envFactorsData } from './components/env/EnvReducers'
 
 var rootReducer = combineReducers({
   routing, fhirPatientData, envFactorsData
