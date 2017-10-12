3afb153542a2bf431d79f1a8a9bb9195193c7daa
Samson Mataraso <samson.mataraso@berkeley.edu>
2017-10-12 00:31:48 -0700

    intermediate reorg

---
+++ deprecated/Relevant_Conditions.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { getValueQuantities, searchByCode, pullCondition } from '../../services/risk_score_utils.js';
+import $ from 'jquery';
+
+const riskObjectConditions = {
+		"General_Cardiac": ['266897007'],
+        "Stroke": ['42343007','38341003','27550009','73211009','230690007', "266257000", "13713005"],
+      //       var chf = pullCondition(conds, ["42343007"]); //byCodes only works w LOINC
+		    // var hypertension = pullCondition(conds, ["38341003"]);
+		    // var vascDisease = pullCondition(conds, ["27550009"]);
+		    // var diabetes = pullCondition(conds, ["73211009"]);
+		    // var strTIAthrom = pullCondition(conds, ["230690007", "266257000", "13713005"]);
+        "Kidney_Failure": [],
+        "COPD_Mortality": ["40917007"], //confusion
+        "Diabetes": ['80394007'] //hyperglycemia
+	};
+
+export class RelevantConditions extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			conds: [<li key={'none'}>None</li>]
+		};
+	}
+
+	componentDidMount(){
+		var codeList = riskObjectConditions[this.props.riskName];
+		var parentComponent = this;
+		$.when(this.props.conditions).done(function(conds) {
+			if(codeList.length != 0) {
+				var condObj = pullCondition(conds, codeList);
+				const condNames = [];
+				for (var i in condObj) {
+					condNames.push(condObj[i].code.text);
+				}
+				if (condNames.length == 0) {
+					condNames.push("None");
+				}
+				const listItems = condNames.map((condName) =>
+					<li key={condName}>{condName}</li>
+				);
+				parentComponent.setState({
+					conds:listItems
+				});
+			}
+		});
+	}
+
+	componentWillReceiveProps(nextProps) {
+		var parentComponent = this;
+		$.when(nextProps.conditions).done(function(conds) {
+			var codeList = riskObjectConditions[nextProps.riskName];
+			if(codeList.length != 0) {
+				var condObj = pullCondition(conds, codeList);
+				const condNames = [];
+				for (var i in condObj) {
+					condNames.push(condObj[i].code.text);
+				}
+				if (condNames.length == 0) {
+					condNames.push("None");
+				}
+				const listItems = condNames.map((condName) =>
+					<li key={condName}>{condName}</li>
+				);
+				parentComponent.setState({
+					conds:listItems
+				});
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<text style={{fontSize: "30"}}> Relevant Conditions </text>
+				<div style={{fontSize: "14"}}><ol>{this.state.conds}</ol></div>
+			</div>
+		);
+	}
+}
+
+export default RelevantConditions;
---
+++ deprecated/Smoking_Tile.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { getValueQuantities, searchByCode, pullCondition } from '../../services/general_utils.js';
+import $ from 'jquery';
+
+const riskObjectConditions = {
+		"General_Cardiac": ['266897007'],
+        "Stroke": ['42343007','38341003','27550009','73211009','230690007', "266257000", "13713005"],
+      //       var chf = pullCondition(conds, ["42343007"]); //byCodes only works w LOINC
+		    // var hypertension = pullCondition(conds, ["38341003"]);
+		    // var vascDisease = pullCondition(conds, ["27550009"]);
+		    // var diabetes = pullCondition(conds, ["73211009"]);
+		    // var strTIAthrom = pullCondition(conds, ["230690007", "266257000", "13713005"]);
+        "Kidney_Failure": [],
+        "COPD_Mortality": ["40917007"], //confusion
+        "Diabetes": ['80394007'] //hyperglycemia
+	};
+
+export class SmokingTile extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	// componentDidMount(){
+	// 	var codeList = riskObjectConditions[this.props.riskName];
+	// 	var parentComponent = this;
+	// 	$.when(this.props.conditions).done(function(conds) {
+
+	// 	});
+	// }
+
+	// componentWillReceiveProps(nextProps) {
+	// 	var parentComponent = this;
+	// 	$.when(nextProps.conditions).done(function(conds) {
+
+	// 	});
+	// }
+
+	render() {
+		return (
+			<div>
+				<Marks xFill={this.props.xFill} cFill={this.props.cFill} onClick={this.props.onClick} smoker={this.props.smoker}/>
+			</div>
+		);
+	}
+}
+
+class Marks extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			smoker: props.smoker
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<text style={{fontSize: "30"}}>Smoking Status</text>
+				<svg onClick={this.props.onClick} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" transform="translate(-40, -20) scale(0.5)">
+					<polygon fill={this.props.xFill} points="96,14 82,0 48,34 14,0 0,14 34,48 0,82 14,96 48,62 82,96 96,82 62,48 "/>
+					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="100" y="-50"
+	 					 viewBox="300 170 500 500" >
+	 					<g transform="scale(2)">
+							<path fill={this.props.cFill} d="M223.9,329.7c-2.4,2.4-5.8,4.4-8.8,4.4s-6.4-2.1-8.9-4.5l-56-56l17.8-17.8l47.2,47.2l124.8-125.7l17.5,18.1L223.9,329.7z"/>
+						</g>
+					</svg>
+				</svg>
+				
+			</div>
+		);
+	}
+}
+
+
+export default SmokingTile;
