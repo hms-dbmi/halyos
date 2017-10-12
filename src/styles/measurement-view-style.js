8ab7fe367834ca786f07a710b5263a72752dcc55
Samson Mataraso <samson.mataraso@berkeley.edu>
2017-10-12 00:28:28 -0700

    intermediate reorg

---
+++ src/styles/measurement-view-style.js
@@ -0,0 +1,27 @@
+export const measurementTitleStyle = {
+	fontSize: "28",
+	fontFamily: "HiraKakuStd-W8, Hiragino Kaku Gothic Std"
+}
+
+export const affectedRiskScoreTitleStyle = {
+	fontSize: "20",
+	fontFamily: "HiraKakuStd-W8, Hiragino Kaku Gothic Std"
+}
+
+export const measurementDetailsHeaderStyle = {
+	fontSize: 18,
+	fontFamily:"Rubik",
+	color:"#666666"
+}
+
+export const measurementDetailsSubheadingStyle = {
+	fontSize: 16,
+	fontFamily:"Rubik",
+	color:"#666666"
+}
+
+export const measurementDetailTextStyle = {
+	fontSize: 14,
+	fontFamily:"Rubik",
+	color:"#666666"
+}
