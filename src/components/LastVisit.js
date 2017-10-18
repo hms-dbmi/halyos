4a3ce53206309edc867cef16e19f6ec2a18ea356
Samson Mataraso <samson.mataraso@berkeley.edu>
2017-10-18 00:27:10 -0700

    Formatted header

--- src/components/LastVisit.js
+++ src/components/LastVisit.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import Calendar from './logos/calendar.js';
 export default class LastVisit extends Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		const mostRecentEncounter = this.props.encounters[0];
-		const mostRecentDate = mostRecentEncounter.effectiveDateTime;
+		if(this.props.encounters) {
+			const mostRecentEncounter = this.props.encounters[0];
+			const mostRecentDate = mostRecentEncounter.effectiveDateTime;
+			return (
+				<div style={{"display":"flex", "flex-direction":"row", "align-items":"center", "padding-top":"10px"}}>
+					Last Visit: {mostRecentDate} &nbsp;&nbsp; <Calendar/>
+				</div>
+			);
+		}
 		return (
-			<div>
-				Last Visit: {mostRecentDate} icon
-			</div>
+			<div> Loading error... </div>
 		);
 	}
 
