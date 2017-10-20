a7d604093e24a3594d2b7b0e575ebae18f6482b8
Fritz Lekschas <service@lekschas.de>
2017-10-20 19:24:24 -0400

    More cleanup

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -8,46 +8,46 @@ import Icon from './Icon';
 import './Topbar.css';
 
 class Header extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { observations: null };
   }
 
   componentWillMount() {
-    //a Set is used below for unique observation codes, could be {} probably.
+    // a Set is used below for unique observation codes, could be {} probably.
     this.observationList = [];
     this.props.ptapi.fetchAll({
-      type: "Observation",
+      type: 'Observation',
       query: {
-        $sort: [['code','asc']],
-        _elements:['code']
-      }
+        $sort: [['code', 'asc']],
+        _elements: ['code'],
+      },
     })
-      .done(data => {
-        let observationList = new Set();
-        let updatedList = []
-        for (let i = 0; i < data.length; i++){
-          //adding stringified text so they can be compared for equality and we keep all the info, just JSON parse it
-          if(data[i].component) {
+      .done((data) => {
+        const observationList = new Set();
+        const updatedList = [];
+        for (let i = 0; i < data.length; i++) {
+          // Adding stringified text so they can be compared for equality and we
+          // keep all the info, just JSON parse it
+          if (data[i].component) {
             for (let comp of data[i].component) {
-              let name = JSON.stringify(comp.code.coding[0]);
+              const name = JSON.stringify(comp.code.coding[0]);
               observationList.add(name);
             }
           } else {
-            let name = JSON.stringify(data[i].code.coding[0]);
+            const name = JSON.stringify(data[i].code.coding[0]);
             observationList.add(name);
           }
         }
 
-        for (let item of observationList){
-          let parsedItem = JSON.parse(item);
+        for (let item of observationList) {
+          const parsedItem = JSON.parse(item);
           if (!(parsedItem.code === '48643-1' || parsedItem.code === '48642-3')) {
             updatedList.push(parsedItem);
           }
         }
 
-        this.setState({ observations:updatedList })
+        this.setState({ observations: updatedList });
       });
   }
 
@@ -64,7 +64,7 @@ class Header extends React.Component {
           </NavLink>
           <NavLink
             to="/about"
-            className="topbar-element"
+            className="topbar-element topbar-element-subtle"
             activeClassName="is-active"
           >
             About
@@ -72,7 +72,7 @@ class Header extends React.Component {
         </nav>
         <nav className="flex-c">
           <div className="topbar-element topbar-last-visit flex-c flex-v-center">
-            <label className="topbar-element-label">Last Visit</label>
+            <div className="topbar-element-subtle">Last Visit</div>
             <date>8/18/16</date>
             <Icon id="calendar"/>
           </div>
@@ -83,52 +83,6 @@ class Header extends React.Component {
         </nav>
       </header>
     );
-
-    // return (
-    //   <Navbar collapseOnSelect className="topbar">
-    //     <Navbar.Header>
-    //       <Navbar.Brand>
-    //         <a href="/"><Icon id="logo"/></a>
-    //       </Navbar.Brand>
-    //       <Navbar.Toggle />
-    //     </Navbar.Header>
-    //     <Navbar.Collapse>
-    //       { !this.state.observations ? (
-    //         <Nav>
-    //           <NavDropdown key={3} title="Measurements" id="basic-nav-dropdown">
-    //             <LinkContainer to="/"><MenuItem key={3.1}>Loading...</MenuItem></LinkContainer>
-    //           </NavDropdown>
-    //           <NavDropdown key={4} title="Risk Scores" id="basic-nav-dropdown">
-    //             <MenuItem key={4.2}>Cardiavascular Disease</MenuItem>
-    //             <MenuItem key={4.3}>Stroke</MenuItem>
-    //             <MenuItem key={4.4}>Kidney Failure</MenuItem>
-    //             <MenuItem key={4.5}>COPD</MenuItem>
-    //             <MenuItem key={4.6}>Diabetes</MenuItem>
-    //           </NavDropdown>
-    //           <LinkContainer to={'/about'}><MenuItem key={5.1}>About</MenuItem></LinkContainer>
-    //         </Nav>
-    //       ) : (
-    //         <Nav>
-    //           <NavDropdown key={5} title="Measurements" id="basic-nav-dropdown">
-    //             {
-    //               this.state.observations.map(obs =>
-    //                 <LinkContainer key={obs.code.toString()} to={`/measure/${obs.code}`}><MenuItem key={obs.code.toString()}>{obs.display}</MenuItem></LinkContainer>
-    //               )
-    //             }
-    //           </NavDropdown>
-    //           <NavDropdown key={6} title="Risk Scores" id="basic-nav-dropdown">
-    //             <LinkContainer to={'/risk/General_Cardiac'}><MenuItem key={6.1}>Cardiavascular Disease</MenuItem></LinkContainer>
-    //             <LinkContainer to={'/risk/Kidney_Failure'}><MenuItem key={6.2}>Kidney Failure</MenuItem></LinkContainer>
-    //             <LinkContainer to={'/risk/COPD_Mortality'}><MenuItem key={6.4}>COPD</MenuItem></LinkContainer>
-    //             <LinkContainer to={'/risk/Stroke'}><MenuItem key={6.5}>Stroke</MenuItem></LinkContainer>
-    //             <LinkContainer to={'/risk/Diabetes'}><MenuItem key={6.6}>Diabetes</MenuItem></LinkContainer>
-    //           </NavDropdown>
-    //           <LinkContainer to={'/about'}><MenuItem key={5.1}>About</MenuItem></LinkContainer>
-    //         </Nav>
-    //       )}
-    //     </Navbar.Collapse>
-    //   </Navbar>
-    // );
   }
 }
 
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,53 +1,45 @@
 import React from 'react';
 
 // Components
-import VitalTile from '../components/VitalTile';
-import { FilteredList, List } from '../components/FilteredList';
-import EnvironmentTile from '../components/EnvironmentFactors';
+import FilteredList from '../components/FilteredList';
 import AppointmentsTile from '../components/AppointmentsTile';
-import Name from '../components/Name';
-import LastVisit from '../components/LastVisit';
 import PollenContainer from '../components/env/PollenContainer';
 import AirQuality from '../components/env/AirQuality';
 import Flu from '../components/env/Flu';
-import PastGraph from '../components/Graphs/Past-Graph';
 import RiskTile from '../components/RiskTile';
-import HelpRiskTile from '../components/RiskTiles/HelpRiskTile';
-import { AboutMeasurement } from '../components/AboutMeasurement';
-import { AboutRisk } from '../components/AboutRisk';
 
 // Services
-import { reynoldsScore } from '../services/RiskCalculators/reynolds';
-import { CHADScore } from '../services/RiskCalculators/CHAD';
-import { KFScore } from '../services/RiskCalculators/get_KFRisk';
-import { COPDScore } from '../services/RiskCalculators/COPD';
-import { diabetesScore } from '../services/RiskCalculators/get_diabetes';
-import { getPtLoc } from '../services/Environment/environmental_utils';
 import { getPatID } from '../services/smart_setup';
 
 // Styles
 import { envTileStyle } from '../styles/Environment-style';
 import './Dashboard.css';
 
-const ptLoc = {
-  "country_code": "US",
-  "region_code": "MA",
-  "city": "Pepperell",
-  "zip_code": "01463",
-  "latitude": 42.669838,
-  "longitude": -71.5961267
-};
-
-const patient = {
-  "gender": "Male",
-  "birthDate":'1979-02-03-12:45'
-};
-
 const measurements = [
-  {name: "Systolic Blood Pressure", units: "mmHg", past: "120", present: "110" },
-  {name: "Diastolic Blood Pressure", units: "mmHg", past: "90", present: "95" },
-  {name: "Heart Rate", units: "bpm", past: "90", present: "70" },
-  {name: "Respiration Rate", units: "breaths/min", past: "18", present: "18" }
+  {
+    name: 'Systolic Blood Pressure',
+    units: 'mmHg',
+    past: '120',
+    present: '110',
+  },
+  {
+    name: 'Diastolic Blood Pressure',
+    units: 'mmHg',
+    past: '90',
+    present: '95',
+  },
+  {
+    name: 'Heart Rate',
+    units: 'bpm',
+    past: '90',
+    present: '70',
+  },
+  {
+    name: 'Respiration Rate',
+    units: 'breaths/min',
+    past: '18',
+    present: '18',
+  },
 ];
 
 class Dashboard extends React.Component {
@@ -56,17 +48,15 @@ class Dashboard extends React.Component {
   }
 
   render() {
-    if (this.props.isFetchingAllPatientData || !this.props.patient){
-      return (
-        <div>Loading...</div>
-      )
+    if (this.props.isFetchingAllPatientData || !this.props.patient) {
+      return <div>Loading...</div>;
     }
 
     let lat;
     let long;
 
-    if(this.props.patient.address[0].extension[0].url.endsWith("geolocation")){
-      if (this.props.patient.address[0].extension[0].extension[0].url === "latitude"){
+    if (this.props.patient.address[0].extension[0].url.endsWith('geolocation')) {
+      if (this.props.patient.address[0].extension[0].extension[0].url === 'latitude') {
         lat = this.props.patient.address[0].extension[0].extension[0].valueDecimal;
         long = this.props.patient.address[0].extension[0].extension[1].valueDecimal;
       } else {
@@ -78,32 +68,54 @@ class Dashboard extends React.Component {
       // likewise, vice versa, given lat and long, go get the location info
     }
 
-    const ptLoc = {"country_code": this.props.patient.address[0].country,
-                 "region_code":this.props.patient.address[0].state,
-                 "city":this.props.patient.address[0].city,
-                 "zip_code": this.props.patient.address[0].postalCode,
-                 "latitude": lat,
-                 "longitude": long,
-               }
+    const ptLoc = {
+      country_code: this.props.patient.address[0].country,
+      region_code: this.props.patient.address[0].state,
+      city: this.props.patient.address[0].city,
+      zip_code: this.props.patient.address[0].postalCode,
+      latitude: lat,
+      longitude: long,
+    };
 
-    const patient = {"gender": this.props.patient.gender, "birthDate":this.props.patient.birthDate};
-    const measurements = [{"name": "Systolic Blood Pressure", "units": "mmHg", "past": "120", "present": "110", "future":"110" },
-    {"name": "Diastolic Blood Pressure", "units": "mmHg", "past": "90", "present": "95", future:95 },
-    {"name": "Heart Rate", "units": "bpm", "past": "90", "present": "70" , future: 80},
-    {"name": "Respiration Rate", "units": "breaths/min", "past": "18", "present": "18" , future:17}]
-    const graphData = [{x:new Date("2017-02-03"), y:124}, {x:new Date("2017-02-12"), y:120}, {x:new Date("2017-02-15"), y:119},
-    {x:new Date("2017-02-23"), y:132}, {x:new Date("2017-03-03"), y:126}, {x:new Date("2017-03-23"), y:129}, {x:new Date("2017-04-03"), y:125}];
-    const mappedMeasures = measurements.map((measurements) =>
-      <tr className = "pure-table pure-table-horizontal">
-        <td> {measurements["name"]} [{measurements["units"]}] </td>
-        <td> {measurements["past"]}</td>
-        <td> {measurements["present"]}</td>
-        <td> {measurements["future"]} </td>
+    const patient = {
+      gender: this.props.patient.gender,
+      birthDate: this.props.patient.birthDate
+    };
+
+    const graphData = [{
+      x: new Date('2017-02-03'),
+      y: 124
+    }, {
+      x: new Date('2017-02-12'),
+      y: 120
+    }, {
+      x: new Date('2017-02-15'),
+      y: 119
+    }, {
+      x: new Date('2017-02-23'),
+      y: 132
+    }, {
+      x: new Date('2017-03-03'),
+      y: 126
+    }, {
+      x: new Date('2017-03-23'),
+      y: 129
+    }, {
+      x: new Date('2017-04-03'),
+      y: 125
+    }];
+
+    const mappedMeasures = measurements.map(measurements => (
+      <tr className = 'pure-table pure-table-horizontal'>
+        <td>{measurements.name} [{measurements.units}]</td>
+        <td>{measurements.past}</td>
+        <td>{measurements.present}</td>
+        <td>{measurements.future}</td>
       </tr>
-    );
+    ));
 
     return (
-      <div className="dashboard">
+      <div className="dashboard full-dim flex-c flex-col">
         <ul className="dashboard-risk-scores flex-c no-list-style">
           <li className="flex-g-1">
             <RiskTile
@@ -152,7 +164,7 @@ class Dashboard extends React.Component {
           </li>
         </ul>
 
-        <div>
+        <div className="dashboard-bottom flex-g-1">
           <div className="pure-u-1-2">
             <FilteredList measurements={measurements}/>
           </div>
@@ -180,17 +192,6 @@ class Dashboard extends React.Component {
               </div>
             </div>
           </div>
-        </div>
-        <div className="pure-u-1-2">
-          <PastGraph
-            obs_data={graphData}
-            units="mmHg"
-            reference_range={{ min: 110, max: 130 }}
-            mainWidth={500}
-            mainHeight={200}
-            viewWidth={500}
-            viewHeight={50}
-          />
         </div>
       </div>
     );
