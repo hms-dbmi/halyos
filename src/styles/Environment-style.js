1c3bbe55de6e8fbb59d04840ac39ac6003ac6688
Fritz Lekschas <service@lekschas.de>
2017-10-22 15:13:56 -0400

    Cleanup. Convert to space indentations. Remove trailing white spaces

--- src/styles/Environment-style.js
+++
@@ -1,8 +0,0 @@
-export const envTileStyle = {
-	"background-color":'lightgrey',
-	"border":"2px solid white",
-	"height": "120px",
-	"width":"120px",
-	"justify-content":"center",
-	"align-items":"center",
-}
