003500dcf359fc24fa0e5ff0520f7f75088f013d
Fritz Lekschas <service@lekschas.de>
2017-10-23 19:54:48 -0400

    Include expandable measurement with fake graph

--- src/components/Measurement.js
+++ src/components/Measurement.js
@@ -15,44 +15,96 @@ const getArrowDir = (past, present) => (past !== present
 
 const getMirrorH = (past, present) => past > present;
 
-const VitalTile = props => (
-  <div className="measurement pure-g">
-    <div className="pure-u-15-24">
-      <div className="full-wh flex-c flex-v-center">
-        <div className="measurement-title">{props.name}</div>
-        <div className="measurement-unit">[{props.unit}]</div>
-      </div>
-    </div>
-    <div className="measurement-past pure-u-2-24 flex-c flex-v-center">
-      {props.past}
-    </div>
-    <div className="measurement-past-to-future pure-u-1-24 flex-c flex-v-center">
-      <Icon
-        id={getArrowDir(props.past, props.present)}
-        mirrorH={getMirrorH(props.past, props.present)}
-      />
-    </div>
-    <div className="measurement-present pure-u-3-24 flex-c flex-v-center">
-      {props.present}
-    </div>
-    <div className="measurement-future pure-u-3-24 flex-c flex-v-center">
-      {props.future}
-    </div>
-    {false && (
-      <PastGraph
-        obs_data={{}}
-        units="mmHg"
-        reference_range={{ min: 110, max: 130 }}
-        mainWidth={500}
-        mainHeight={200}
-        viewWidth={500}
-        viewHeight={50}
-      />
-    )}
-  </div>
-);
+const GRAPH_DATA = [
+  {
+    x: new Date('2017-02-03'),
+    y: 124
+  }, {
+    x: new Date('2017-02-12'),
+    y: 120
+  }, {
+    x: new Date('2017-02-15'),
+    y: 119
+  }, {
+    x: new Date('2017-02-23'),
+    y: 132
+  }, {
+    x: new Date('2017-03-03'),
+    y: 126
+  }, {
+    x: new Date('2017-03-23'),
+    y: 129
+  }, {
+    x: new Date('2017-04-03'),
+    y: 125
+  }
+];
 
-VitalTile.propTypes = {
+class Measurement extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDetailsShown: false
+    };
+  }
+
+  showDetails() {
+    this.setState({
+      isDetailsShown: !this.state.isDetailsShown
+    });
+  }
+
+  render() {
+    return (
+      <div className="measurement">
+        <div className="measurement-info pure-g">
+          <div className="pure-u-15-24">
+            <div className="full-wh flex-c flex-v-center">
+              <div
+                className="measurement-title p"
+                onClick={() => this.showDetails()}
+              >
+                {this.props.name}
+              </div>
+              <div className="measurement-unit">[{this.props.unit}]</div>
+            </div>
+          </div>
+          <div className="measurement-past pure-u-2-24 flex-c flex-v-center">
+            {this.props.past}
+          </div>
+          <div className="measurement-past-to-future pure-u-1-24 flex-c flex-v-center">
+            <Icon
+              id={getArrowDir(this.props.past, this.props.present)}
+              mirrorH={getMirrorH(this.props.past, this.props.present)}
+            />
+          </div>
+          <div className="measurement-present pure-u-3-24 flex-c flex-v-center">
+            {this.props.present}
+          </div>
+          <div className="measurement-future pure-u-3-24 flex-c flex-v-center">
+            {this.props.future}
+          </div>
+        </div>
+        <div className="measurement-graph">
+          {this.state.isDetailsShown && (
+            <PastGraph
+              obs_data={GRAPH_DATA}
+              units="mmHg"
+              reference_range={{ min: 110, max: 130 }}
+              mainWidth={500}
+              mainHeight={200}
+              viewWidth={500}
+              viewHeight={50}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+Measurement.propTypes = {
   name: PropTypes.string,
   unit: PropTypes.string,
   past: PropTypes.string,
@@ -60,4 +112,4 @@ VitalTile.propTypes = {
   future: PropTypes.string,
 };
 
-export default VitalTile;
+export default Measurement;
