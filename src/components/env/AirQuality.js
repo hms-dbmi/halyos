cfde8e7f3aeb1a44779a0b6bc829aca7bb32dd58
Samson Mataraso <samson.mataraso@berkeley.edu>
2017-10-31 01:18:13 -0700

    implemented forms of static data w/ other environmental tiles to help with formatting

--- src/components/env/AirQuality.js
+++ src/components/env/AirQuality.js
@@ -4,6 +4,9 @@ import React from 'react';
 // Components
 import EnvironmentTile from '../EnvironmentTile';
 
+//Static Data
+import {airQualityLocal} from '../../data/fhirData';
+
 const AirQuality = props => (
   <EnvironmentTile
     name="Air Quality"
@@ -11,15 +14,23 @@ const AirQuality = props => (
     icon="air"
     isCollapsed={props.isCollapsed}
     isExpanded={props.isExpanded}
-    level={props.airQuality}
+    level={"Good"}
+    /*level={props.airQuality}*/
   >
+  {console.log(airQualityLocal.data)}
     <div>
       <h5>Air Quality (AQI) Near You</h5>
       <div>
         <h6>Main Pollutant</h6>
-        <div>{props.pollutant}</div>
+        <div>
+          {/*{props.pollutant}*/}
+          {airQualityLocal.data.current.pollution.mainus}
+        </div>
         <h6>Air Quality Level</h6>
-        <div>{props.airQualityExplanation}</div>
+        <div>
+          {/*{props.airQualityExplanation}*/}
+          The air quality index is {airQualityLocal.data.current.pollution.aqius}. This means air quality is considered satisfactory, and air pollution poses little or no risk.
+        </div>
       </div>
     </div>
   </EnvironmentTile>
--- src/components/env/Flu.js
+++ src/components/env/Flu.js
@@ -4,19 +4,28 @@ import React from 'react';
 // Components
 import EnvironmentTile from '../EnvironmentTile';
 
+//Static Data
+import {fluLocal} from '../../data/fhirData';
+
 const Flu = props => (
   <EnvironmentTile
     name="Flu"
     expand={props.expand}
     isCollapsed={props.isCollapsed}
     isExpanded={props.isExpanded}
-    level={props.level}
+    level={"Low"}
+    /*level={props.level}*/
   >
     <div>
       <h5>Mentions of Flu Symptoms Near You</h5>
-      <p>{props.destination}</p>
+      <p>
+        {/*{props.destination}*/}
+      </p>
       <h5>Explanation</h5>
-      <p>{props.levelExplanation}</p>
+      <p>
+        {/*{props.levelExplanation}*/}
+        There are a few cases of flu in your area. You might not need the seasonal vaccine, but if it is flu season, check with your healthcare provider.
+      </p>
     </div>
   </EnvironmentTile>
 );
