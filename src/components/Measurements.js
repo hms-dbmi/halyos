77f86c16290367bc68e583a2255d7af0fe92455f
Fritz Lekschas <service@lekschas.de>
2017-11-01 18:04:40 -0400

    Integrate and style date picker

--- src/components/Measurements.js
+++ src/components/Measurements.js
@@ -1,16 +1,27 @@
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import React from 'react';
+import DatePicker from 'react-datepicker';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 // Components
+import Button from './Button';
+import Icon from './Icon';
 import Measurement from './Measurement';
+
+// Actions
+import { setPastDate } from '../actions';
 
 // Styles
 import './Measurements.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class Measurements extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isDatePickerShown: false,
       measurements: this.props.measurements
     };
   }
@@ -22,9 +33,24 @@ class Measurements extends React.Component {
     });
   }
 
+  toggleDatePicker() {
+    this.setState({
+      isDatePickerShown: !this.state.isDatePickerShown
+    });
+  }
+
+  pastChangeHandler(date) {
+    this.toggleDatePicker();
+    this.props.setPastDate(date.unix() * 1000);
+  }
+
   render() {
+    const pastDate = moment(this.props.pastDate || undefined);
+
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAA', pastDate);
+
     return (
-      <div className="measuremrents full-wh flex-c flex-col">
+      <div className="measurements full-wh flex-c flex-col">
         <header className="dashboard-panel-headline pure-g flex-c flex-align-sb">
           <div className="pure-u-15-24">
             <div className="flex-c flexc-v-center">
@@ -37,7 +63,11 @@ class Measurements extends React.Component {
               />
             </div>
           </div>
-          <div className="pure-u-3-24 smaller">
+          <div
+            className="pure-u-3-24 smaller flex-c flex-v-center"
+            onClick={this.toggleDatePicker.bind(this)}
+          >
+            <Icon id="calendar"/>
             Past
           </div>
           <div className="pure-u-3-24 smaller">
@@ -48,6 +78,24 @@ class Measurements extends React.Component {
           </div>
         </header>
         <main className="measurements-list flex-c flex-col flex-g-1 r scrollbar">
+          {this.props.pastDate && (
+            <div className="dashboard-panel-subtopbar">
+              <div className="pure-u-15-24"></div>
+              <div className="pure-u-3-24">
+                {this.props.pastDate && (
+                  <div className="flex-c flex-v-center">
+                    <span>{pastDate.format('MM/DD/YY')}</span>
+                    <Button
+                      icon="cross"
+                      onClick={() => { this.props.setPastDate(null); }}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="pure-u-3-24"></div>
+              <div className="pure-u-3-24"></div>
+            </div>
+          )}
           {this.state.measurements.map((item, index) => (
             <Measurement
               key={index}
@@ -59,6 +107,17 @@ class Measurements extends React.Component {
             />
           ))}
         </main>
+        {this.state.isDatePickerShown && (
+          <DatePicker
+            selected={pastDate}
+            onChange={this.pastChangeHandler.bind(this)}
+            onClickOutside={this.toggleDatePicker.bind(this)}
+            monthsShown={2}
+            fixedHeight
+            withPortal
+            inline
+          />
+        )}
       </div>
     );
   }
@@ -70,6 +129,20 @@ Measurements.propTypes = {
   isCollapsed: PropTypes.bool,
   isExpanded: PropTypes.bool,
   measurements: PropTypes.array,
+  pastDate: PropTypes.number,
+  setPastDate: PropTypes.func,
 };
 
-export default Measurements;
+const mapStateToProps = state => ({
+  pastDate: state.pastDate,
+});
+
+const mapDispatchToProps = dispatch => ({
+  setPastDate: pastDate =>
+    dispatch(setPastDate(pastDate)),
+});
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Measurements));
