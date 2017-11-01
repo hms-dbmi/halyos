014f31402e40e343d05eeefefdb03d1e1b748e04
Fritz Lekschas <service@lekschas.de>
2017-11-01 18:03:54 -0400

    Add pastDate to redux store

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
+
+import defaultSetReducer from './utils/default-set-reducer';
+
 import { fhirPatientData, fhirObservationData, fhirEncounterData } from './services/fhir/FhirReducers';
 import { envFactorsData } from './components/env/EnvReducers';
+
+export const pastDate = defaultSetReducer('pastDate', Date.now());
 
 const rootReducer = combineReducers({
   routing,
@@ -9,6 +14,7 @@ const rootReducer = combineReducers({
   fhirObservationData,
   envFactorsData,
   fhirEncounterData,
+  pastDate
 });
 
 export default rootReducer;
