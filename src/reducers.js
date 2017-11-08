3e7bd6a22da5a8f79b7016698508df888f2edb46
Vimig Socrates <vimig.socrates@gmail.com>
2017-11-08 05:07:09 -0500

    change default past date for demo

--- src/reducers.js
+++ src/reducers.js
@@ -6,7 +6,7 @@ import defaultSetReducer from './utils/default-set-reducer';
 import { fhirPatientData, fhirObservationData, fhirEncounterData } from './services/fhir/FhirReducers';
 import { envFactorsData } from './components/env/EnvReducers';
 
-export const pastDate = defaultSetReducer('pastDate', Date.now());
+export const pastDate = defaultSetReducer('pastDate', new Date(2012, 7, 15));
 
 const rootReducer = combineReducers({
   routing,
