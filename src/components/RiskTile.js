7d90e42a41bd6055967919b64a6782b54d4ad8f6
Fritz Lekschas <service@lekschas.de>
2017-11-10 14:00:28 -0500

    Fix context bar

--- src/components/RiskTile.js
+++ src/components/RiskTile.js
@@ -11,7 +11,7 @@ const CONTEXT_MAX = 10;
 
 const getLength = (context, contextMax) => ({
   width: `${100 * context / (contextMax || CONTEXT_MAX)}%`,
-  height: `${(contextMax || CONTEXT_MAX) - context}px`,
+  height: `${(contextMax || CONTEXT_MAX) - context + 1}px`,
   bottom: `-${(contextMax || CONTEXT_MAX) - context}px`
 });
 
@@ -24,7 +24,7 @@ const RiskTile = props => (
       <h2 className="risk-tile-title">{props.name}</h2>
       <div className="risk-tile-status">{props.status}</div>
       <div className="risk-tile-score flex-c flex-align-c flex-v-bottom">
-        <div className="risk-tile-score-value">{(props.periodOfTime==="Present" || props.periodOfTime===null) ? props.score : props.futureScore(props.presentMeasurements, props.futureMeasurements, props.data.patient, props.data.conditions, props.data.medications)}</div>
+        <div className="risk-tile-score-value">{(props.periodOfTime === "Present" || props.periodOfTime === null) ? props.score : props.futureScore(props.presentMeasurements, props.futureMeasurements, props.data.patient, props.data.conditions, props.data.medications)}</div>
         <div className="risk-tile-score-unit">{props.unit}</div>
       </div>
       <div className="flex-c flex-align-sb">
