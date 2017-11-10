953633969d54792b06fda572bd26c9d5b20fd444
Samson Mataraso <samson.mataraso@berkeley.edu>
2017-11-10 11:11:25 -0800

    moved futurescore calculation into risktilecontainer

--- src/components/RiskTileContainer.js
+++ src/components/RiskTileContainer.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import RiskTile from './RiskTile';
-
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
+	var futureScoreValue = ownProps.futureScore(state.measurementState.presentMeasurements, state.measurementState.futureMeasurements, ownProps.data.patient, ownProps.data.conditions, ownProps.data.medications)
 	return {
 		futureMeasurements: state.measurementState.futureMeasurements,
 		presentMeasurements: state.measurementState.presentMeasurements,
-		periodOfTime: state.measurementState.periodOfTime
+		periodOfTime: state.measurementState.periodOfTime,
+		futureScoreValue: futureScoreValue
 	}
 }
 
