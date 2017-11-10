6938816b5c844b153e0d5bb4a1609937e1e59df4
Fritz Lekschas <service@lekschas.de>
2017-11-10 15:48:23 -0500

    Adjusted styling for past and future risk scores

--- src/components/RiskTileContainer.js
+++ src/components/RiskTileContainer.js
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux';
 import RiskTile from './RiskTile';
-const mapStateToProps = (state, ownProps) => {
-	var futureScoreValue = ownProps.futureScore(state.measurementState.presentMeasurements, state.measurementState.futureMeasurements, ownProps.data.patient, ownProps.data.conditions, ownProps.data.medications)
-	return {
-		futureMeasurements: state.measurementState.futureMeasurements,
-		presentMeasurements: state.measurementState.presentMeasurements,
-		periodOfTime: state.measurementState.periodOfTime,
-		futureScoreValue: futureScoreValue
-	}
-}
+
+const mapStateToProps = (state, ownProps) => ({
+  futureMeasurements: state.measurementState.futureMeasurements,
+  presentMeasurements: state.measurementState.presentMeasurements,
+  periodOfTime: state.measurementState.periodOfTime,
+  futureScore: ownProps.futureScore(
+    state.measurementState.presentMeasurements,
+    state.measurementState.futureMeasurements,
+    ownProps.data.patient,
+    ownProps.data.conditions,
+    ownProps.data.medications
+  )
+});
 
 const RiskTileContainer = connect(
   mapStateToProps
