a01f5baf5aa5d552adebde5a66c1653e8d04d1d7
Samson Mataraso <samson.mataraso@berkeley.edu>
2017-12-05 23:39:40 -0800

    allows graph to always be rendered

--- src/components/Measurement.js
+++ src/components/Measurement.js
@@ -43,7 +43,6 @@ class Measurement extends React.Component {
   }
 
   showDetails() {
-    if (this.props.past && this.props.pastDate) {
       if (this.props.risk) {
         this.props.expandAbout(false, !this.state.isDetailsShown && this.props.name)
         this.setState({
@@ -55,10 +54,6 @@ class Measurement extends React.Component {
         });
         this.props.expandAbout(this.state.isDetailsShown, this.props.name);
       }
-    }
-    else {
-      alert("Details cannot be displayed, as the patient only has one measurement.")
-    }
   }
 
   futureChangeHandler(newValue) {
