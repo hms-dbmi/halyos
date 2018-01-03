4a3b3ff9433172df8ed67b91bd96549786c92290
Samson Mataraso <samson.mataraso@berkeley.edu>
2018-01-03 01:27:44 -0800

    updating pastgraph

--- src/components/PastGraph.js
+++ src/components/PastGraph.js
@@ -62,7 +62,7 @@ class PastGraph extends React.Component {
           className="past-graph"
           ref={(elem) => { this.baseEl = elem; }}
         />
-        <button type="button" onClick={setPresent.bind(this)}> Reset to present value </button>
+        <button type="button" onClick={setPresent.bind(this)}> Reset to present valueg </button>
       </div>
     );
   }
