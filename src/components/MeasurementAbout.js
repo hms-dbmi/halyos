cb6acb6bb7b5f594ece144bd161b8f35062b7d81
Fritz Lekschas <service@lekschas.de>
2018-04-02 11:50:48 -0400

    Clean up and add scrollbar style

--- src/components/MeasurementAbout.js
+++ src/components/MeasurementAbout.js
@@ -13,7 +13,10 @@ import text from '../texts/measurementText';
 const MeasurementAbout = props => (
   <div className="measurement-about full-wh flex-c flex-col">
     <header className="dashboard-panel-headline flex-c flex-align-sb">
-      <h3 className="dashboard-panel-headline-no-colon" style={{fontSize:"16px"}}>About: {props.name || "Click on a measurement to learn more!"}</h3>
+      <h3
+        className="dashboard-panel-headline-no-colon"
+        style={{fontSize:"16px"}}>About: {props.name || "Click on a measurement to learn more!"}
+      </h3>
       {props.isExpanded && (
         <Button
           icon="cross"
@@ -22,12 +25,12 @@ const MeasurementAbout = props => (
         />
       )}
     </header>{!props.name ||
-    <div className="flex-g-1 r">
-      <h4><strong>What</strong> does my {props.name} mean?</h4>
+    <div className="flex-g-1 r scrollbar">
+      <h4>What does my <em>{props.name}</em> mean?</h4>
       <p>{(props.name && text[props.name] && text[props.name].meaning) || 'Not available...'}</p>
-      <h4><strong>Why</strong> is my {props.name} important?</h4>
+      <h4>Why is my <em>{props.name}</em> important?</h4>
       <p>{(props.name && text[props.name] && text[props.name].important) || 'Not available...'}</p>
-      <h4><strong>How</strong> can I make it better?</h4>
+      <h4>How can I make it better?</h4>
       <p>{(props.name && text[props.name] && text[props.name].improve) || 'Not available...'}</p>
     </div>}
   </div>
@@ -37,7 +40,7 @@ MeasurementAbout.propTypes = {
   expand: PropTypes.func,
   isCollapsed: PropTypes.bool,
   isExpanded: PropTypes.bool,
-  name: PropTypes.string, 
+  name: PropTypes.string,
   what: PropTypes.string,
   why: PropTypes.string,
   how: PropTypes.string,
