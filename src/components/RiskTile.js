ab77682c0b2b956cc39eb93610d4d908c9ad1529
Fritz Lekschas <service@lekschas.de>
2018-06-05 10:50:41 -0400

    Move risk score context (years) underneath the title. Add some hover effect

--- src/components/RiskTile.js
+++ src/components/RiskTile.js
@@ -136,21 +136,23 @@ class RiskTile extends React.Component {
           onClick={() => { this.props.expand(this.props.name); }}
         >
           <h2 className="risk-tile-title">{this.props.name} Risk</h2>
-          <div className="risk-tile-status">{this.props.status}</div>
+          <div className="flex-c flex-align-c risk-tile-context">
+            {`within ${this.props.context} ${(this.props.context === 1) ? "year" : "years"}`}
+          </div>
           <div className="risk-tile-score flex-c flex-align-c">
-
             {this.props.pastScore &&
               <div className="flex-c flex-align-c flex-v-bottom risk-tile-score-past">
                 <div className="risk-tile-score-value">{this.props.pastScore}
                 </div>
                 <div className="risk-tile-score-unit">{this.props.unit}</div>
               </div>
-            } &nbsp;&nbsp;&nbsp;
+            }
 
             <div className="flex-c flex-align-c flex-v-bottom risk-tile-score-present">
               <div className="risk-tile-score-value">{this.props.score}</div>
               <div className="risk-tile-score-unit">{this.props.unit}</div>
-            </div> &nbsp;&nbsp;&nbsp;
+            </div>
+
             {this.props.futureScore &&
               <div className="flex-c flex-align-c flex-v-bottom risk-tile-score-future">
                 <div className="risk-tile-score-value">{this.props.futureScore}</div>
@@ -158,15 +160,6 @@ class RiskTile extends React.Component {
               </div>
             }
           </div>
-          <div className="flex-c flex-align-sb">
-            <div className="risk-tile-context">{`within ${this.props.context} ${(this.props.context == 1) ? "year" : "years"}`}</div>
-            {/* <Button
-              icon="info"
-              iconOnly={true}
-              className="risk-tile-info"
-              onClick={() => alert('More info on the risk here.')} /> */}
-          </div>
-         {/* <div className="risk-tile-context-bar" style={getLength(this.props.context, this.props.contextMax)}></div>*/}
         </div>
       }
       </div>
@@ -181,7 +174,6 @@ RiskTile.propTypes = {
   isCollapsed: PropTypes.bool,
   isExpanded: PropTypes.bool,
   name: PropTypes.string,
-  status: PropTypes.string,
   unit: PropTypes.string,
 };
 
